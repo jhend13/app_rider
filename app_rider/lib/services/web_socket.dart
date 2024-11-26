578bab2920086929ac09a7b857c9e39a7004af89
Justin Hendrix <jhendrix13@gmail.com>
2024-11-25 21:35:39 -0800

    small changes to WebSocketService

---
+++ app_rider/lib/services/web_socket.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:app_rider/config/constants.dart' as constants;
+
+class WebSocketService {
+  final WebSocketChannel _socket = WebSocketChannel.connect(
+    Uri.parse(constants.urlWebSocketEndpoint),
+  );
+
+  WebSocketService();
+
+  Stream<dynamic> get stream => _socket.stream;
+
+  void sendJson(Map<String, dynamic> data) {
+    String json = jsonEncode(data);
+    _socket.sink.add(json);
+  }
+
+  void close() {
+    _socket.sink.close();
+  }
+}
