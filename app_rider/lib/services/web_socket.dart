300b5dbb1568956011b2cd1d8cc6cfbb20f16f55
Justin Hendrix <jhendrix13@gmail.com>
2024-11-29 15:03:02 -0800

    added reconnection logic to WebSocketService

--- app_rider/lib/services/web_socket.dart
+++ app_rider/lib/services/web_socket.dart
@@ -3,20 +3,54 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:app_rider/config/constants.dart' as constants;
 
 class WebSocketService {
-  final WebSocketChannel _socket = WebSocketChannel.connect(
-    Uri.parse(constants.urlWebSocketEndpoint),
-  );
+  static const _reconnectCooldown = 5; // seconds
+  bool _isConnecting = false;
+  WebSocketChannel? _socket;
 
-  WebSocketService();
+  Stream<dynamic> get stream => _socket?.stream ?? const Stream.empty();
 
-  Stream<dynamic> get stream => _socket.stream;
+  WebSocketService() {
+    _connect();
+  }
+
+  void _connect() {
+    if (_isConnecting) return;
+
+    _isConnecting = true;
+
+    try {
+      _socket = WebSocketChannel.connect(
+        Uri.parse(constants.urlWebSocketEndpoint),
+      );
+
+      _socket!.ready.then((val) {
+        _isConnecting = false;
+      }, onError: (err) {
+        _isConnecting = false;
+        _reconnect();
+      });
+
+      _socket!.stream.listen((data) {}, onError: (err) {}, onDone: () {
+        _reconnect();
+      });
+    } catch (e) {
+      _isConnecting = false;
+      _reconnect();
+    }
+  }
+
+  void _reconnect() {
+    Future.delayed(const Duration(seconds: _reconnectCooldown), () {
+      _connect();
+    });
+  }
 
   void send(Map<String, dynamic> data) {
     String json = jsonEncode(data);
-    _socket.sink.add(json);
+    _socket?.sink.add(json);
   }
 
   void close() {
-    _socket.sink.close();
+    _socket?.sink.close();
   }
 }
