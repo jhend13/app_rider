1017ad5a2500610afced583f963b9079d70def46
Justin Hendrix <jhendrix13@gmail.com>
2024-12-04 21:44:11 -0800

    listeners created to receive fcm msgs/notifications

--- app_rider/lib/services/notification.dart
+++ app_rider/lib/services/notification.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class NotificationService {
@@ -10,16 +11,31 @@ class NotificationService {
     _init();
   }
 
+  // firebase package dictates that onBackgroundMessage
+  // needs to be a non-anonymous, top-level function.
+  static Future<void> _onBackgroundMessage(RemoteMessage msg) async {
+    print('{fb.onBackgroundMessage} ${DateTime.now()}');
+    print(msg.data);
+  }
+
   void _init() async {
     notificationSettings =
         await FirebaseMessaging.instance.requestPermission(provisional: true);
 
     String? _fcmToken = await FirebaseMessaging.instance.getToken();
 
+    // to-do: .onError
     FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
       _fcmToken = fcmToken;
-    }).onError((err) {
-      // Error getting token.
     });
+
+    // when app is opened
+    FirebaseMessaging.onMessage.listen((RemoteMessage msg) {
+      print('{fb.onMessage} test message received');
+      print(msg.data);
+    });
+
+    // when app is in background / closed
+    FirebaseMessaging.onBackgroundMessage(_onBackgroundMessage);
   }
 }
